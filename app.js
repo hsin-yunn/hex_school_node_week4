eb0b4912af8cd4ee47e076253ffbe7d6d45261d2
hsinyun <ruby8941@gmail.com>
2022-05-04 23:04:58 +0800

    [issue] add connect mongoose

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+//env
+dotenv.config({ path: './config.env' });
+const DB = env.process.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD,
+);
+//連接資料庫
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('connect success');
+  })
+  .catch((err) => {
+    console.log('connect faild', err);
+  });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
